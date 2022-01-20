1cc006a8d609d7c0b45daf51fb72261f44e4ff1f
cahitsahin <sahincahit.cs@gmail.com>
2022-01-20 19:52:05 +0100

    Fix UI

--- flutter-users/flutteruser/lib/screens/home.dart
+++ flutter-users/flutteruser/lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutteruser/blocs/user_provider.dart';
 import 'package:flutteruser/models/user.dart';
 import 'package:flutteruser/screens/user.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -22,15 +23,34 @@ class _HomePageState extends State<HomePage> {
       builder: (context, result) {
         if (result.hasData){
           users = result.data;
+          users!.sort((b,a) => a.createdAt.compareTo(b.createdAt));
           return ListView.builder(
           itemCount: users!.length,
           itemBuilder: (context, index) {
+            DateTime time = DateTime.parse(result.data?[index].createdAt ?? "");
+            String formattedDate = DateFormat('yyyy-MM-dd, kk:mm:ss').format(time);
+            String name = result.data?[index].firstName  ?? "" ;
+            String surName = result.data?[index].lastName  ?? "" ;
+            String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
+            name = capitalize(name);
+            surName = capitalize(surName);
             return Card(
               child: ListTile(
-                title: Text(result.data?[index].firstName ?? "Blank"),
-                leading: CircleAvatar(child: Icon(Icons.account_circle),),
-                subtitle: Text(result.data?[index].lastName ?? "Blank"),
-                trailing: Text(result.data?[index].id.toString() ?? "Blank"),
+                title: Row(
+                  children: [
+                    Text(name),
+                    const SizedBox(width: 10,),
+                    Text(surName,style: TextStyle(color: Colors.grey[600]),)
+                  ],
+                ),
+                leading: const CircleAvatar(child: Icon(Icons.account_circle),),
+                subtitle: Text(formattedDate),
+                trailing: GestureDetector(
+                  onTapDown: (TapDownDetails details) {
+                    _showPopupMenu(details.globalPosition);
+                  },
+                  child: const Icon(Icons.settings),
+                ),
                 onTap: () {
                   Navigator.push(
                       context,
@@ -59,4 +79,22 @@ class _HomePageState extends State<HomePage> {
 
     super.initState();
   }
+
+  void _showPopupMenu(Offset offset) async {
+    double left = offset.dx;
+    double top = offset.dy;
+    await showMenu(
+      context: context,
+
+      position: RelativeRect.fromLTRB(left, top, 0, 0),
+      items: [
+        const PopupMenuItem<String>(
+            child:  Icon(Icons.edit),),
+        const PopupMenuItem<String>(
+            child:  Icon(Icons.delete,color: Colors.redAccent,)),
+      ],
+      elevation: 8.0,
+    );
+  }
+
 }
